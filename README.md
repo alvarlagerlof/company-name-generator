5f18f81a299de7d768e3b577c3040050e98453f8
Alvar Lagerlöf <alvarlagerlof@gmail.com>
2020-12-27 18:16:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Comany name generator
+Name genertor using Markov chains via the Foswig libraray
+
+## Running
+Edit the settings object in index.js, then do
+
+`npm install`
+`npm run generate`
---
+++ index.js
@@ -0,0 +1,105 @@
+import Foswig from "foswig";
+import whois from "whois";
+import chalk from "chalk";
+import readline from "readline";
+import syllable from "syllable";
+
+import dictionary from "./dictionary.js";
+import companies from "./companies.js";
+import fortune from "./fortune.js";
+import angel from "./angel.js";
+import keywords from "./keywords.js";
+
+const setup = {
+	words: [...angel, ...fortune, ...keywords],
+	randomness: 3,
+	constraints: {
+		minLength: 6,
+		maxLength: 8,
+		allowDuplicates: true,
+	},
+	maxSyllables: 4,
+	waitTime: 100,
+	debug: false,
+};
+
+// Create the Markov chain and specify the order of the chain & input dictionary
+// The order (an integer that is greater than 0) indicates how many previous
+// letters are taken into account when selecting the next one. A smaller order
+// will result in a more randomized, less recognizeable output. Also, a higher
+// order will result in words which resemble more closely to those in the original
+// dictionary.
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+const error = chalk.bold.red;
+const warning = chalk.hex("#ffbe0a");
+const success = chalk.hex("00FF00");
+
+const chain = new Foswig(setup.randomness, setup.words);
+
+async function run() {
+	while (true) {
+		await showWord();
+	}
+}
+
+function showWord() {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const name = await chain.generate(setup.constraints);
+
+			if (syllable(name) < setup.maxSyllables && (await isAvailable(name))) {
+				console.log(success(name + ".com"));
+				resolve();
+			} else {
+				console.log(warning(name + ".com"));
+				reject(new Error("Not fullilling the requirements"));
+			}
+		} catch (e) {
+			reject(e);
+		}
+	}).catch((e) => {
+		if (setup.debug) console.log(error(e));
+	});
+}
+
+function isAvailable(name) {
+	return new Promise(async (resolve, reject) => {
+		await new Promise((resolve) => setTimeout(resolve, setup.waitTime));
+
+		await whois.lookup(name + ".com", function (err, data) {
+			if (setup.debug) console.log(data && data.substring(0, 70));
+
+			if (
+				data &&
+				(data.includes("IP Address Has Reached Rate Limit") ||
+					data.includes("Exceeded max command rate") ||
+					data.includes("Too many queries from your IP"))
+			) {
+				console.log(error("Rate limit exceded"));
+				setTimeout(() => {
+					reject(new Error("Rate limit exceded"));
+				}, 10000);
+			} else if (
+				data &&
+				data.includes(`No match for domain "${name.toUpperCase()}.COM"`)
+			) {
+				resolve(true);
+			} else {
+				reject(new Error("Domain not available"));
+			}
+		});
+	}).catch((e) => {
+		if (setup.debug) console.log(error(e));
+	});
+}
+
+process.stdin.on("keypress", (str, key) => {
+	if (key.ctrl && key.name === "c") {
+		process.exit();
+	}
+});
+
+run();
